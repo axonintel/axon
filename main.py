18500c22c4a4b14fafeed4f86791d9f8edf41a5b
Axon <40429952+axonintel@users.noreply.github.com>
2021-09-28 20:14:48 -0500

    Add files via upload
    main websocket client for axon

---
+++ main.py
@@ -0,0 +1,29 @@
+import websocket
+import json
+import logging
+import warnings
+from os import path
+
+
+def on_message(wsapp, message):
+    try:
+        msg = json.loads(message)
+        print(msg)
+    except Exception as e:
+        print("Caught exception when converting message into json\n" + str(e))
+
+
+def main():
+
+    logging.basicConfig(format='{asctime} {name} {threadName} {levelname}: {message}', style='{', level=logging.INFO)
+    warnings.filterwarnings("ignore")
+
+    BASE_DIR = path.dirname(path.dirname(path.abspath(__file__)))  # (str): absolute path to project root
+
+    uri = "wss://api.axonintellex.com"
+    wsapp = websocket.WebSocketApp(uri, on_message=on_message)
+    wsapp.run_forever(ping_interval=60)
+
+
+if __name__ == "__main__":
+    main()
