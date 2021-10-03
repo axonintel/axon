ce0d75916e80f7bcf1beb4b7c87fc861528c1d21
axon <support@axonintellex.com>
2021-10-02 23:11:52 -0500

    added timestamps

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import websocket
 import json
 import logging
 import warnings
+import time
 from os import path
+
+timestamp = int(time.time())
 
 
 def on_message(wsapp, message):
     try:
         msg = json.loads(message)
-        print(msg)
+        ts = msg['timestamp']
+        print(msg) if is_new_message(ts) else print("Older forecast: ", msg)
     except Exception as e:
         print("Caught exception when converting message into json\n" + str(e))
 
@@ -25,6 +29,21 @@ def main():
     header = {"x-api-key":trial_key}
     wsapp = websocket.WebSocketApp(uri, on_message=on_message, header=header)
     wsapp.run_forever(ping_interval=60)
+
+
+def is_new_message(ts):
+    """
+    Compares timestamps of the incoming message from the websocket to determine if a new notification came in.
+    Sets global timestamp to the newest incoming timestamp
+    :param ts: integer  timestamp from incoming notification
+    :return: True or False
+    """
+    global timestamp
+    if ts > timestamp:
+        timestamp = ts
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
