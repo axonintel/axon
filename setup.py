c3bca2a8b95bcdfbdbfc14121c6e1b05f45bddf0
axon <support@axonintellex.com>
2021-10-06 21:39:59 -0500

    updated config file for 0.0.8 release

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='axonbot',
-    version='0.0.7',
+    version='0.0.8',
     author='Shehadi Dayekh',
     author_email='shehadi@axonintellex.com',
     license='MIT',
