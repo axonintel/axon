deb0ebb22edc755ac2f4a32fd2c85533e2ed6d69
axon <support@axonintellex.com>
2021-11-15 11:18:21 -0600

    version 0.2.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='axonbot',
-    version='0.2.0',
+    version='0.2.1',
     author='Shehadi Dayekh',
     author_email='shehadi@axonintellex.com',
     license='MIT',
